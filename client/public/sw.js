0e40d384007d6e306cb743f1f049f24ef0f4a533
RuSail Developer <rusail@example.com>
2025-02-25 19:37:47 +0000

    Update configuration and paths for GitHub Pages

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'rusail-app-v1';
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon.svg',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  '.',
+  'index.html',
+  'manifest.json',
+  'icons/icon.svg',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
