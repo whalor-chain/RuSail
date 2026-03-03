73539430febdb688bdff3d0f880200eba96b486c
Whalor <stasagunker@gmailcom>
2026-03-03 05:27:03 +0300

    Initial Commit

---
+++ RuSail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RuSail
+//
+//  Created by Станислава Гункер on 03.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RuSail/RuSailApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuSailApp.swift
+//  RuSail
+//
+//  Created by Станислава Гункер on 03.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RuSailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
